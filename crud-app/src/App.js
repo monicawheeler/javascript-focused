3e58375227e800ffb214d00696b167b9250df4fa
Monica Wheeler <mwheeler2@apple.com>
2020-11-29 12:31:44 -0600

    update heading

--- crud-app/src/App.js
+++ crud-app/src/App.js
@@ -38,7 +38,7 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1>CRUD App with Hooks</h1>
+      <h1>User Table</h1>
       <div className="flex-row">
         <div className="flex-large">
         {editing ? (
